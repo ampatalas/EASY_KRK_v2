c2b8e4b9e3436492e2d3a9c5db05b1eca29e50db
ampatalas <a.m.patalas@gmail.com>
2016-01-24 12:12:50 +0100

    wyświetlanie listy kategorii

--- EASY_KRK/Controllers/KategoriaController.cs
+++ EASY_KRK/Controllers/KategoriaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EASY_KRK.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,11 +10,16 @@ namespace EASY_KRK.Controllers
     [Authorize]
     public class KategoriaController : Controller
     {
+        EASYKRKContext db = new EASYKRKContext();
         //
         // GET: /Kategoria/
         public ActionResult Index()
         {
-            return View();
+            KategorieViewModel Model = new KategorieViewModel();
+            var IdProgramu = Convert.ToInt32(this.HttpContext.Session["IdProgramu"]);
+            Model.Kategorie = db.Kategorie.Where(k => k.ProgramStudiow.IdProgramuKsztalcenia == IdProgramu
+                                                        && k.KategoriaNadrzedna == null);
+            return View(Model);
         }
 	}
 }
---
+++ EASY_KRK/Models/KategorieViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EASY_KRK.Models
+{
+    public class KategorieViewModel
+    {
+        public IEnumerable<Kategoria> Kategorie { get; set; }
+    }
+}
